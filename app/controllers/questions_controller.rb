8c5d062e36d1b2be4713bf083d32be7ece401b82
Cristian Manolache <mlcristian@outlook.com>
2024-01-28 22:52:07 +0000

    v1.0 Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,14 +4,26 @@ class QuestionsController < ApplicationController
   def answer
     if params[:question].present?
       @question = params[:question]
+    #   if @question == "I'am going to work" || @question == "I'am going to work".downcase
+    #     @answer = "Great!"
+    #   elsif @question == "I am going to work" || @question == "I am going to work".downcase
+    #     @answer = "Great!"
+    #   elsif @question.end_with?("?")
+    #     @answer = "Silly question, get dressed and go to work!"
+    #   else
+    #     @answer = "I don't care, get dressed and go to work!"
+    #   end
+    # else
+    #   @answer = "It looks like you didn't ask anything."
 
-      if @question == "I am going to work"
-        @answer = "Great!"
-      elsif @question.end_with?("?")
-        @answer = "Silly question, get dressed and go to work!"
-      else
-        @answer = "I don't care, get dressed and go to work!"
-      end
+    case @question.downcase
+    when "i am going to work", "i'm going to work"
+      @answer = "Great!"
+    when /\?$/
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
     else
       @answer = "It looks like you didn't ask anything."
     end
